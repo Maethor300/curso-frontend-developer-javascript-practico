49949799e47daca73d840bc113c9d91fd2a42f07
David Mendoza Erazo <davidleomendoza300@gmail.com>
2023-04-12 12:11:16 -0500

    Update mostrar primero menu lado izquierdo

---
+++ main.js
@@ -0,0 +1,5 @@
+const nav1 = document.querySelector(".inactive")
+const boton = document.querySelector(".boton1")
+boton.addEventListener("click" , function(){
+      nav1.classList.toggle("inactive")
+})
