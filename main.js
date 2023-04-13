3f3b7ce95e7d289172820fc00001465cd222439d
David Mendoza Erazo <davidleomendoza300@gmail.com>
2023-04-13 18:01:03 -0500

    Push mobile-menu responsive

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
-const nav1 = document.querySelector(".inactive")
+const nav1 = document.querySelector(".desktop-menu")
 const boton = document.querySelector(".boton1")
 boton.addEventListener("click" , function(){
       nav1.classList.toggle("inactive")
+})
+const mobileMenu = document.querySelector(".mobile-menu")
+const menuBoton = document.querySelector(".menu")
+menuBoton.addEventListener("click", function(){
+      mobileMenu.classList.toggle("inactive")
 })
